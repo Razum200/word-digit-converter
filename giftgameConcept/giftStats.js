9146f4461363f6419024f8849bf0efc6aeea15f7
Razum200 <razum2000@icloud.com>
2025-05-07 06:07:41 +0300

    Update giftStats.js

--- giftgameConcept/giftStats.js
+++ giftgameConcept/giftStats.js
@@ -1,11 +1,39 @@
 const glsStats = {
-    BEAST: { attack: 5, defense: 5, hp: 3 },
-    JOBS:  { attack: 2,  defense: 10, hp: 3 },
-    GLASS: { attack: 1,  defense: 1, hp: 11 },
-    LEET:  { attack: 3,  defense: 7, hp: 3 },
-    BLAZE: { attack: 3,  defense: 3, hp: 3 },
-    CUBE:  { attack: 3,  defense: 3, hp: 3 },
-    STONE: { attack: 3,  defense: 3, hp: 3 },
-    WIND:  { attack: 4,  defense: 2, hp: 3 },
-    VOID:  { attack: 0,  defense: 0, hp: 9 }
+    BEAST: { attack: 3, defense: 1, hp: 0 },
+    JOBS:  { attack: 1,  defense: 3, hp: 0 },
+    GLASS: { attack: 2,  defense: 0, hp: 1 },
+    LEET:  { attack: 3,  defense: 3, hp: 0 },
+    BLAZE: { attack: 2,  defense: 2, hp: 0 }
+  };
+
+  const nftStats = {
+    PlushPepe: { attack: 3, defense: 3, hp: 3 },
+    TamaGadget:  { attack: 1,  defense: 1, hp: 1 },
+    SleighBall: { attack: 1,  defense: 0, hp: 0 },
+    CandyCane:  { attack: 1,  defense: 2, hp: 3 },
+    SpicedWine: { attack: 1,  defense: 0, hp: 3 }
+  };
+
+  const modelStats = {
+    Squid: { attack: 3, defense: 3, hp: 3 },
+    Spades:  { attack: 1,  defense: 1, hp: 1 },
+    Golden: { attack: 1,  defense: 0, hp: 0 },
+    Candyno:  { attack: 1,  defense: 2, hp: 3 },
+    RickMorty: { attack: 1,  defense: 0, hp: 3 }
+  };
+
+  const patternStats = {
+    Pencil: { attack: 3, defense: 3, hp: 3 },
+    Tablet:  { attack: 1,  defense: 1, hp: 1 },
+    Ball: { attack: 1,  defense: 0, hp: 0 },
+    Octo:  { attack: 1,  defense: 2, hp: 3 },
+    Wine: { attack: 1,  defense: 0, hp: 3 }
+  };
+
+  const bgStats = {
+    Gray: { attack: 0, defense: 1, hp: 1 },
+    Black:  { attack: 2,  defense: 2, hp: 0 },
+    Blue: { attack: 1,  defense: 0, hp: 1 },
+    Rose:  { attack: 1,  defense: 2, hp: 0 },
+    Violet: { attack: 1,  defense: 0, hp: 0 }
   };
