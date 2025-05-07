7ec79cdb5c28cd072c0a6391fa96874180a665f1
Razum200 <razum2000@icloud.com>
2025-05-07 09:40:14 +0300

    Update giftStats.js

--- giftgameConcept/giftStats.js
+++ giftgameConcept/giftStats.js
@@ -1,39 +1,39 @@
 const glsStats = {
-    BEAST: { attack: 3, defense: 1, hp: 0 },
+    BEAST: { attack: 3, defense: 2, hp: 0 },
     JOBS:  { attack: 1,  defense: 3, hp: 0 },
-    GLASS: { attack: 2,  defense: 0, hp: 1 },
-    LEET:  { attack: 3,  defense: 3, hp: 0 },
-    BLAZE: { attack: 2,  defense: 2, hp: 0 }
+    GLASS: { attack: 2,  defense: 0, hp: 2 },
+    LEET:  { attack: 2,  defense: 2, hp: 0 },
+    BLAZE: { attack: 2,  defense: 1, hp: 0 } /*25*/ 
   };
 
   const nftStats = {
-    PlushPepe: { attack: 3, defense: 3, hp: 3 },
+    PlushPepe: { attack: 3, defense: 0, hp: 0 },
     TamaGadget:  { attack: 1,  defense: 1, hp: 1 },
-    SleighBall: { attack: 1,  defense: 0, hp: 0 },
-    CandyCane:  { attack: 1,  defense: 2, hp: 3 },
-    SpicedWine: { attack: 1,  defense: 0, hp: 3 }
+    SleighBall: { attack: 1,  defense: 2, hp: 0 },
+    CandyCane:  { attack: 1,  defense: 2, hp: 0 },
+    SpicedWine: { attack: 0,  defense: 0, hp: 3 } /* 18 */
   };
 
   const modelStats = {
-    Squid: { attack: 3, defense: 3, hp: 3 },
+    Squid: { attack: 0, defense: 0, hp: 3 },
     Spades:  { attack: 1,  defense: 1, hp: 1 },
-    Golden: { attack: 1,  defense: 0, hp: 0 },
-    Candyno:  { attack: 1,  defense: 2, hp: 3 },
-    RickMorty: { attack: 1,  defense: 0, hp: 3 }
+    Golden: { attack: 1,  defense: 0, hp: 2 },
+    Candyno:  { attack: 1,  defense: 2, hp: 0 },
+    RickMorty: { attack: 1,  defense: 0, hp: 2 } /* 18 */
   };
 
   const patternStats = {
-    Pencil: { attack: 3, defense: 3, hp: 3 },
-    Tablet:  { attack: 1,  defense: 1, hp: 1 },
+    Pencil: { attack: 1, defense: 0, hp: 0 },
+    Tablet:  { attack: 0,  defense: 1, hp: 0 },
     Ball: { attack: 1,  defense: 0, hp: 0 },
-    Octo:  { attack: 1,  defense: 2, hp: 3 },
-    Wine: { attack: 1,  defense: 0, hp: 3 }
+    Octo:  { attack: 1,  defense: 0, hp: 0 },
+    Wine: { attack: 1,  defense: 0, hp: 0 } /* 6 */
   };
 
   const bgStats = {
     Gray: { attack: 0, defense: 1, hp: 1 },
-    Black:  { attack: 2,  defense: 2, hp: 0 },
+    Black:  { attack: 0,  defense: 2, hp: 0 },
     Blue: { attack: 1,  defense: 0, hp: 1 },
-    Rose:  { attack: 1,  defense: 2, hp: 0 },
-    Violet: { attack: 1,  defense: 0, hp: 0 }
+    Rose:  { attack: 1,  defense: 1, hp: 0 },
+    Violet: { attack: 1,  defense: 1, hp: 0 } /* 12 */
   };
